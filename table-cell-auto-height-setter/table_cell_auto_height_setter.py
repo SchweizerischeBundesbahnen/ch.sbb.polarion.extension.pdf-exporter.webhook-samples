d04b9472b293145a1d0c40cd94208102c02c6f3c
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-09-02 09:20:55 +0200

    chore: optional basic auth refactored

--- table-cell-auto-height-setter/table_cell_auto_height_setter.py
+++ table-cell-auto-height-setter/table_cell_auto_height_setter.py
@@ -3,16 +3,31 @@ import logging
 import re
 
 from flask import Flask, Response, request, json, abort
+from flask_httpauth import HTTPBasicAuth
 from gevent.pywsgi import WSGIServer
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+users = {
+    "username1": generate_password_hash("userpass1"),
+    "username2": generate_password_hash("userpass2")
+}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+    return None
+
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] - %(message)s')
 
 
 @app.route("/td-height/<height>", methods=["POST"])
+@auth.login_required
 def process_html(height):
-    authorize()
     export_params, html = validate_request()
 
     logging.info(f"Changing HTML table cell height to '{height}'")
@@ -29,15 +44,6 @@ def process_html(height):
 def change_height(html, height):
     pattern = r'(<t[dh].+?height:.*?)(\d+(\.\d+)?px)'
     return re.sub(pattern, lambda m: m.group(1) + height, html)
-
-
-def authorize():
-    if args.username and args.password:
-        auth = request.authorization
-        if not auth:
-            abort(Response('No authorization data', 401))
-        if auth.username != args.username or auth.password != args.password:
-            abort(Response('Invalid username/password provided', 401))
 
 
 def validate_request():
